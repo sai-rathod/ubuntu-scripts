0330af215d7602731c557279e7925ce986490111
sai-rathod <sahilrathodrk31@gmail.com>
2025-05-19 15:41:17 +0530

    Initial commit from ubuntu

---
+++ pdfbrutforce.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#brutforcing the pdf 
+
+#trying to access file via command line
+path1="/home/saii/pdf"
+path2="/home/saii/pdf/unlocked"
+file="Sayindra-DevOps_protected.pdf"
+protected_file="$path1/$file"
+#trying unlocking with knownpasswd
+
+
+for (( i=0; i<=999999; i++ ))
+do 
+
+#trying decrypting
+output=$(qpdf --password=$i --show-encryption "$path2/$RANDOM.pdf" &> /dev/null | grep "I")
+
+#matching the output
+
+if [[ $output != "I" ]];then
+	echo "password found:$i"
+exit 0
+fi
+
+done
+echo " password not found in this range "
+exit 1
