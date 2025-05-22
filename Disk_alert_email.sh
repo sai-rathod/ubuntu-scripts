a4f7d444503380c777012c58ff190cb3e4d721ea
sai-rathod <sahilrathodrk31@gmail.com>
2025-05-22 21:36:52 +0530

    auto committed on date Thu May 22 09:36:52 PM IST 2025

--- Disk_alert_email.sh
+++ Disk_alert_email.sh
@@ -5,7 +5,7 @@
 FU=$(df -hT | grep "sda2" |awk '{print $6}' | tr -d %)
 TO="www.rahulroy7878@gmail.com"
 
-if [[ $FU -ge 30 ]];then
+if [[ $FU -ge 80 ]];then
 	echo "Disk space is running low:$FU%" | mail -s "Disk space Alert!!!!" $TO
 else 
 	echo "all good"
