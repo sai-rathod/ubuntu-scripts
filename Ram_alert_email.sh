67b286df6fb2867bd4b02c56e9fb3c76edb226d4
sai-rathod <sahilrathodrk31@gmail.com>
2025-05-23 12:44:20 +0530

    auto committed on date Fri May 23 12:44:20 PM IST 2025

--- Ram_alert_email.sh
+++ Ram_alert_email.sh
@@ -4,7 +4,10 @@
 Ram=$(free -mt | grep "Total" | awk '{print $4}')
 
 TH=5000
-To=www.rahulroy7878@gmail.com
+To="www.rahulroy7878@gmail.com"
+
+echo "available Ram:$Ram"
+echo "available Threashold:$TH"
 
 if [[ $Ram -le $TH ]];then
 	echo "Warning Ram space is running low:$Ram" | mail -s "Ram space alert!!!" $To
